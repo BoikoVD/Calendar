6b6e8211b52811e31865b34d14a92f861b88d821
BoikoVD <boiko.volodymyr.dm@gmail.com>
2021-11-02 01:03:30 +0200

    Replaced inputTime block

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { format, set } from 'date-fns';
+import InputTime from './InputTime';
 
 function Popup() {
 	const dispatch = useDispatch();
@@ -20,12 +21,12 @@ function Popup() {
 
 	function activeDay(date) {
 		setTime(format(date, "kk:mm"));
-		inputTimeRef.current.setAttribute('disabled', 'disabled');
+		inputTimeRef.current.lastChild.setAttribute('disabled', 'disabled');
 		buttonsRef.current.firstChild.classList.remove('_active');
 		buttonsRef.current.lastChild.classList.add('_active');
 	}
 	function disableDay() {
-		inputTimeRef.current.removeAttribute('disabled');
+		inputTimeRef.current.lastChild.removeAttribute('disabled');
 		buttonsRef.current.firstChild.classList.add('_active');
 		buttonsRef.current.lastChild.classList.remove('_active');
 		setTime('');
@@ -35,12 +36,18 @@ function Popup() {
 	};
 	function timeSplit(time) {
 		let timeArr = time.split(/\s*:\s*/);
-		let hourse = Number(timeArr[0]);
+		let hours = Number(timeArr[0]);
 		let minutes = Number(timeArr[1]);
-		return { hourse, minutes };
+		return { hours, minutes };
 	}
 	function setZeroTime(date) {
 		return set(date, { hours: 0, minutes: 0, seconds: 0 });
+	}
+	function getTimes(time) {
+		let thisTime = timeSplit(time);
+		let startTime = timeSplit(myWorkTime.start);
+		let finishTime = timeSplit(myWorkTime.finish);
+		return { thisTime, startTime, finishTime }
 	}
 
 	React.useEffect(() => {
@@ -58,13 +65,21 @@ function Popup() {
 		}
 	}, [date]);
 
+	React.useEffect(() => {
+		let { thisTime, startTime, finishTime } = getTimes(time);
+		if (thisTime.hours < startTime.hours || (thisTime.hours >= finishTime.hours && thisTime.minutes !== 0) || thisTime.hours === NaN) {
+			inputTimeRef.current.classList.add('_invalid');
+		} else {
+			inputTimeRef.current.classList.remove('_invalid');
+		}
+	}, [time]);
+
+
 	const clickOnAddMeeting = () => {
-		let thisTime = timeSplit(time);
-		let startTime = timeSplit(myWorkTime.start);
-		let finishTime = timeSplit(myWorkTime.finish);
-		let thisDate = set(date, { hours: thisTime.hourse, minutes: thisTime.minutes, seconds: 0 });
-		let startDate = set(date, { hours: startTime.hourse, minutes: startTime.minutes, seconds: 0 });
-		let finishDate = set(date, { hours: finishTime.hourse, minutes: finishTime.minutes, seconds: 0 });
+		let { thisTime, startTime, finishTime } = getTimes(time);
+		let thisDate = set(date, { hours: thisTime.hours, minutes: thisTime.minutes, seconds: 0 });
+		let startDate = set(date, { hours: startTime.hours, minutes: startTime.minutes, seconds: 0 });
+		let finishDate = set(date, { hours: finishTime.hours, minutes: finishTime.minutes, seconds: 0 });
 		if (thisDate >= startDate && thisDate <= finishDate) {
 			let newActiveDates = [...activeDates, thisDate];
 			dispatch({ type: "ADD_DATE", payload: newActiveDates });
@@ -98,17 +113,9 @@ function Popup() {
 						<label htmlFor="input-day" className="popup__label">Day</label>
 						<input value={format(date, "do EEEE")} disabled type="text" className="popup__input" id="input-day" />
 					</div>
-					<div className="popup__column">
+					<div className="popup__column popup__column_time" ref={inputTimeRef}>
 						<label htmlFor="input-time" className="popup__label">Time</label>
-						<input value={time}
-							onChange={(e) => setTime(e.target.value)}
-							type="time"
-							min={myWorkTime.start}
-							max={myWorkTime.finish}
-							required
-							className="popup__input"
-							id="input-time"
-							ref={inputTimeRef} />
+						<InputTime time={time} setTime={setTime} />
 					</div>
 					<div className="popup__column" ref={buttonsRef}>
 						<button onClick={clickOnAddMeeting} className="popup__btn popup__add-meeting _active" type="button">Add meeting</button>
